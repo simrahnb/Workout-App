4e930510c96c47cb387eb9ef96f18bd246e8d2d9
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-03-21 11:33:46 +0500

    Added express router with all the API routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts.js");
 
 // express app created
 const app = express();
 
 // middleware - this function will fire for every request
+app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
-});
+}); 
 
 // react to requests so need to set up a route handler
 // GET request sent to this url to the server
 // server responds with this JSON
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the app" });
-});
+// routes
+app.use('/api/workouts', workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
