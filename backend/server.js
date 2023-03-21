fbf625e255f9672875695cc5d64033d57205fc00
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-03-21 12:09:52 +0500

    Set up db using MongoDB Atlas and connected to app via Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const workoutRoutes = require("./routes/workouts.js");
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
-// express app created
-const app = express();
+// express app
+const app = express()
 
-// middleware - this function will fire for every request
+// middleware
 app.use(express.json())
+
 app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-}); 
+  console.log(req.path, req.method)
+  next()
+})
 
-// react to requests so need to set up a route handler
-// GET request sent to this url to the server
-// server responds with this JSON
 // routes
-app.use('/api/workouts', workoutRoutes);
+app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port", process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI )
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
