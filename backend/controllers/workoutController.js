21566363e33219b6f8fc3b6656d8247aa863d707
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-03-21 12:52:57 +0500

    Created a new model& schema for the db collection

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,19 @@
+const Workout = require('../models/workoutModel')
+
+// get all workouts 
+
+
+// get a single workout 
+
+
+
+// create new workout
+
+
+
+// delete a workout 
+
+
+
+// update a workout
+
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,7 @@
 // using the express router
 const express = require("express");
 
+
 const router = express.Router();
 
 // GET all workouts
@@ -14,9 +15,16 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
-  // req.body
-  res.json({ msg: "POST a new workout" });
+router.post("/", async (req, res) => {
+  const {title, load, reps} = req.body
+
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+
 });
 
 // DELETE a workout
