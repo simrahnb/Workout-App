c46f06b561dbc68f073834c0f09ffe2e44f30305
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-03-24 23:43:27 +0500

    formatted code

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,28 @@
 // using the express router
 const express = require("express");
 const {
-    createWorkout, 
-    getWorkout,
-    getWorkouts,
-    deleteWorkout,
-    updateWorkout
-} = require('../controllers/workoutController')
-
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", getWorkouts)
+router.get("/", getWorkouts);
 
 // GET a single workout
-router.get("/:id", getWorkout)
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post("/", createWorkout)
+router.post("/", createWorkout);
 
 // DELETE a workout
-router.delete("/:id", deleteWorkout)
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a workout
-router.patch("/:id", updateWorkout)
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
